7e43725c9eb3845fe83412d7d9f84dd4d27a9ac5
Sokhavuth TIN <vuthdevelop@gmail.com>
2020-11-06 03:49:18 +0700

    Make it better

--- controllers/login.py
+++ controllers/login.py
@@ -97,7 +97,7 @@ class Login(Bottle):
     
     if 'DYNO' in os.environ:
       
-      pdf = pydf.generate_pdf(template, orientation='Lanscape')
+      pdf = pydf.generate_pdf(template, orientation='Landscape')
       with open(rootPath + id+'.pdf', 'wb') as f:
         f.write(pdf)
         f.close()
